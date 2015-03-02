4176dc8aa1b960f63e946486d5084e5b7227fb4c
Mathias Winde <Mapede11@student.aau.dk>
2015-03-02 08:49:26 +0100

    Benytter nu den opdateret version af module_lib

--- app/src/main/java/dk/aau/cs/psylog/psylog_screenonoffmodule/ScreenSensorService.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_screenonoffmodule/ScreenSensorService.java
@@ -1,15 +1,10 @@
 package dk.aau.cs.psylog.psylog_screenonoffmodule;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
 import dk.aau.cs.psylog.module_lib.SuperService;
 
 public class ScreenSensorService extends SuperService {
     @Override
-    public void onCreate()
-    {
-        super.onCreate();
+    public void setSensor() {
         sensor = new ScreenListener(this);
     }
 }
