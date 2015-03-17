55e617d4f6136b11ca6473186a555f137b229789
Mathias Winde <Mapede11@student.aau.dk>
2015-03-17 12:35:45 +0100

    Ændrede sensor til at gøre brug af Supersensor

--- app/src/main/java/dk/aau/cs/psylog/screen/PsyLogService.java
+++ app/src/main/java/dk/aau/cs/psylog/screen/PsyLogService.java
@@ -1,8 +1,8 @@
 package dk.aau.cs.psylog.screen;
 
-import dk.aau.cs.psylog.module_lib.SuperService;
+import dk.aau.cs.psylog.module_lib.SensorService;
 
-public class PsyLogService extends SuperService {
+public class PsyLogService extends SensorService {
     @Override
     public void setSensor() {
         sensor = new ScreenListener(this);
